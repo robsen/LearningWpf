1af2b00db3ba2243564b8df8f1043990328610bf
Robert Jaśkowski <robert.jaskowski@ymail.com>
2020-03-29 20:46:23 +0200

    Add Project SwapCursorByTag
    This project swaps a cursor and saves the currently used cursor in
    FrameworkElement.Tag.

---
+++ SwapCursorByTag/MainWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace SwapCursorByTag
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnChangeCursor(object sender, MouseButtonEventArgs e)
+        {
+            if (sender is TextBlock textBlock)
+                SwapCursor(textBlock);
+        }
+
+        private void SwapCursor(TextBlock textBlock)
+        {
+            if (textBlock != null
+                && textBlock.Tag is Cursor savedCursor)
+            {
+                var currentCursor = textBlock.Cursor as Cursor;
+                textBlock.Cursor = savedCursor;
+                textBlock.Tag = currentCursor;
+            }
+        }
+    }
+}
