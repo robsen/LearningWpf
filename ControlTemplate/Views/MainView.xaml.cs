fed163322ab27737e8c515b7ad2a46467b168b3d
Robert Jaśkowski <robert.jaskowski@ymail.com>
2020-03-30 21:41:05 +0200

    Add Project ControlTemplate
    This project implements the basics of reuseable controls as well as
    keyboard navigation by tabbing.

---
+++ ControlTemplate/Views/MainView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows;
+
+namespace ControlTemplate.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+    }
+}
