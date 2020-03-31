e215e1fe9c493b987750cdb096439246e8648b80
Robert Jaśkowski <robert.jaskowski@ymail.com>
2020-03-31 22:51:07 +0200

    Add Forgotton Files from Project ApplicationClass

---
+++ ApplicationClass/App.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Windows;
+using ApplicationClass.Views;
+
+namespace ApplicationClass
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private void Application_Startup(object sender, StartupEventArgs eventArgs)
+        {
+            Properties.Add("ApplicationStartTimestamp", DateTime.Now);
+
+            var mainWindow = new MainView()
+            {
+                Content = GetApplicationArguments(eventArgs)
+            };
+            mainWindow.Show();
+        }
+
+        private static string GetApplicationArguments(StartupEventArgs e)
+        {
+            return string.Join(" | ", e.Args);
+        }
+
+        private void Application_Exit(object sender, ExitEventArgs e)
+        {
+            TimeSpan duration = CalculateApplicationRunTime();
+            MessageBox.Show(
+                $"Application was running for {duration.Seconds} seconds.", 
+                "Application Total Runtime", 
+                MessageBoxButton.OK, 
+                MessageBoxImage.Information
+            );
+        }
+
+        private TimeSpan CalculateApplicationRunTime()
+        {
+            var startTimestamp = (DateTime)Properties["ApplicationStartTimestamp"];
+            return DateTime.Now - startTimestamp;
+        }
+    }
+}
