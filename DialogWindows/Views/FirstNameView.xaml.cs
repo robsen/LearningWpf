643427994f48fd28b02333ca8855f1fb43ad4eaf
Robert Jaśkowski <robert.jaskowski@ymail.com>
2020-04-01 20:01:50 +0200

    Add Project DialogWindows
    This project demonstrates how to use modal windows and plays around with
    controls focus.

---
+++ DialogWindows/Views/FirstNameView.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace DialogWindows.Views
+{
+    /// <summary>
+    /// Interaction logic for FirstNameView.xaml
+    /// </summary>
+    public partial class FirstNameView : Window
+    {
+        public FirstNameView()
+        {
+            InitializeComponent();
+        }
+
+        public string FirstName { get; set; }
+
+        private void OnSave(object sender, RoutedEventArgs e)
+        {
+            FirstName = FirstNameTextBox.Text;
+            DialogResult = true;
+        }
+    }
+}
---
+++ DialogWindows/Views/MainView.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System.Windows;
+
+namespace DialogWindows.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        //public static FirstNameView RenameDialog { get; set; }
+
+        private void OnRename(object sender, RoutedEventArgs e)
+        { 
+            var renameDialog = new FirstNameView();
+            bool? isRenamed = renameDialog.ShowDialog();
+            if (isRenamed == true)
+                FirstNameTextBlock.Text = 
+                    string.IsNullOrWhiteSpace(renameDialog.FirstName)
+                        ? "Unknown"
+                        : renameDialog.FirstName;
+        }
+    }
+}
