a3264c4e5d9834b49828fc368ff69cc283c08217
Robert Jaśkowski <robert.jaskowski@ymail.com>
2020-04-02 21:22:57 +0200

    Add Project SimpleTexteditor
    This project is a exercise from a udemy course to practice Drop events on
    window and tunneling events. I upgraded the simple task by checking for
    single file only drops with user feedback.

---
+++ SimpleTexteditor/Views/MainView.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.Windows;
+using System.Windows.Threading;
+
+namespace SimpleTexteditor.Views
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void OnDrop(object sender, DragEventArgs e)
+        {
+            if (IsFileDrop(e) == false)
+                return;
+
+            var filePaths = e.Data.GetData(DataFormats.FileDrop) as string[];
+            if (filePaths.Length > 1)
+                ShowMessageBoxFileAmountExceeded();
+            else
+                EditorTextBox.Text = System.IO.File.ReadAllText(filePaths[0]);
+        }
+
+        private static void ShowMessageBoxFileAmountExceeded()
+        {
+            MessageBox.Show(
+                "Only 1 text file can be dropped at once.",
+                "File Amount Exceeded",
+                MessageBoxButton.OK,
+                MessageBoxImage.Information,
+                MessageBoxResult.OK,
+                MessageBoxOptions.DefaultDesktopOnly
+            );
+        }
+
+        private static bool IsFileDrop(DragEventArgs e)
+        {
+            return e.Data.GetDataPresent(DataFormats.FileDrop);
+        }
+
+        private void OnPreviewDragOver(object sender, DragEventArgs e)
+        {
+            e.Effects = DragDropEffects.All;
+            e.Handled = true;
+        }
+    }
+}
