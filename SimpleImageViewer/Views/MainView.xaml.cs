9613c8063ce56748d040782fcde677555ca2faa6
Robert Jaśkowski <robert.jaskowski@ymail.com>
2020-04-05 16:45:11 +0200

    Add Project SimpleImageViewer
    A simple image viewer with image stretching functionality for following image formats: bmp, jpg, jpeg and png.

---
+++ SimpleImageViewer/Views/MainView.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+
+namespace SimpleImageViewer.Views
+{
+    /// <summary>
+    /// Interaction logic for MainView.xaml
+    /// </summary>
+    public partial class MainView : Window
+    {
+        public MainView()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenFileDialog(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = CreateOpenFileDialogForImages();
+            if (IsImageFileSelected(openFileDialog) == true)
+                ShowSelectedImage(openFileDialog);
+        }
+
+        private void ShowSelectedImage(OpenFileDialog openFileDialog)
+        {
+            DisplayedImage.Source =
+                CreateImageFromSelectedImageFile(openFileDialog);
+        }
+
+        private static BitmapImage CreateImageFromSelectedImageFile(OpenFileDialog openFileDialog)
+        {
+            return new BitmapImage(
+                new Uri(
+                    openFileDialog.FileName,
+                    UriKind.Absolute)
+                );
+        }
+
+        private bool? IsImageFileSelected(OpenFileDialog openFileDialog)
+        {
+            return openFileDialog.ShowDialog(this);
+        }
+
+        private static OpenFileDialog CreateOpenFileDialogForImages()
+        {
+            return new OpenFileDialog()
+            {
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures),
+                Filter = "Image Files|*.bmp;*.png;*.jpg;*.jpeg|All files (*.*)|*.*"
+            };
+        }
+    }
+}
